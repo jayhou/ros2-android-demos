d33a3c886ada5bd5b7ac1f816ff18ff0b4a86413
JayHou <houjie@lixiang.com>
2020-05-13 16:21:44 +0800

    ROS2Demo android listener in activity

---
+++ ROS2Demo/app/src/main/java/org/ros2/demos/ListenerNode.java
@@ -0,0 +1,27 @@
+package org.ros2.demos;
+
+import android.os.Bundle;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import org.ros2.rcljava.node.BaseComposableNode;
+import org.ros2.rcljava.subscription.Subscription;
+
+public class ListenerNode extends BaseComposableNode {
+    private final String topic;
+
+    private final TextView listenerView;
+
+    private Subscription<std_msgs.msg.String> subscriber;
+
+    public ListenerNode(final String name, final String topic,
+                        final TextView listenerView) {
+        super(name);
+        this.topic = topic;
+        this.listenerView = listenerView;
+        this.subscriber = this.node.<std_msgs.msg.String>createSubscription(
+                std_msgs.msg.String.class, this.topic, msg
+                        -> Log.d("ListenerNode","Hello ROS2 from Android: " + msg.getData()));
+    }
+}
---
+++ ROS2Demo/app/src/main/java/org/ros2/demos/ROS2ListenerActivity.java
@@ -0,0 +1,115 @@
+/* Copyright 2016-2017 Esteve Fernandez <esteve@apache.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ros2.demos;;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.ros2.rcljava.RCLJava;
+
+import org.ros2.android.activity.ROSActivity;
+
+public class ROS2ListenerActivity extends ROSActivity {
+
+    private static final String IS_WORKING = "isWorking";
+
+    private ListenerNode listenerNode;
+
+    private TextView listenerView;
+
+    private static String logtag = ROS2ListenerActivity.class.getName();
+
+    private boolean isWorking;
+
+    /** Called when the activity is first created. */
+    @Override
+    public final void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        if (savedInstanceState != null) {
+            isWorking = savedInstanceState.getBoolean(IS_WORKING);
+        }
+
+        Button buttonStart = (Button)findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(startListener);
+
+        Button buttonStop = (Button)findViewById(R.id.buttonStop);
+        buttonStop.setOnClickListener(stopListener);
+
+        listenerView = (TextView)findViewById(R.id.listenerView);
+        listenerView.setMovementMethod(new ScrollingMovementMethod());
+
+        RCLJava.rclJavaInit();
+
+        listenerNode =
+                new ListenerNode("android_listener_node", "chatter", listenerView);
+
+        changeState(isWorking);
+    }
+
+    // Create an anonymous implementation of OnClickListener
+    private OnClickListener startListener = new OnClickListener() {
+        public void onClick(final View view) {
+            Log.d(logtag, "onClick() called - start button");
+            Toast.makeText(
+                    ROS2ListenerActivity.this, "The Start button was clicked.",
+                    Toast.LENGTH_LONG).show();
+            Log.d(logtag, "onClick() ended - start button");
+            changeState(true);
+        }
+    };
+
+    // Create an anonymous implementation of OnClickListener
+    private OnClickListener stopListener = new OnClickListener() {
+        public void onClick(final View view) {
+            Log.d(logtag, "onClick() called - stop button");
+            Toast.makeText(
+                    ROS2ListenerActivity.this, "The Stop button was clicked.",
+                    Toast.LENGTH_LONG).show();
+            changeState(false);
+            Log.d(logtag, "onClick() ended - stop button");
+        }
+    };
+
+    private void changeState(boolean isWorking) {
+        this.isWorking = isWorking;
+        Button buttonStart = (Button)findViewById(R.id.buttonStart);
+        Button buttonStop = (Button)findViewById(R.id.buttonStop);
+        buttonStart.setEnabled(!isWorking);
+        buttonStop.setEnabled(isWorking);
+        if (isWorking){
+            getExecutor().addNode(listenerNode);
+        } else {
+            getExecutor().removeNode(listenerNode);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (outState != null) {
+            outState.putBoolean(IS_WORKING, isWorking);
+        }
+        super.onSaveInstanceState(outState);
+    }
+}
+
